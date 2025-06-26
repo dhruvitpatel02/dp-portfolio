d7a1a430712bba1009f0712b4ba86082a74b3dec
dhruvitpatel02 <dhruvit.patel@gmail.com>
2025-06-26 10:00:20 +0530

    Updated files

--- script.js
+++ script.js
@@ -145,22 +145,22 @@ document.querySelectorAll('.skill-item').forEach(item => {
 });
 
 // Project card 3D tilt effect
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('mousemove', function(e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+// document.querySelectorAll('.project-card').forEach(card => {
+//     card.addEventListener('mousemove', function(e) {
+//         const rect = this.getBoundingClientRect();
+//         const x = e.clientX - rect.left;
+//         const y = e.clientY - rect.top;
         
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
+//         const centerX = rect.width / 2;
+//         const centerY = rect.height / 2;
         
-        const rotateX = (y - centerY) / 10;
-        const rotateY = (centerX - x) / 10;
+//         const rotateX = (y - centerY) / 10;
+//         const rotateY = (centerX - x) / 10;
         
-        this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
-    });
+//         this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
+//     });
     
-    card.addEventListener('mouseleave', function() {
-        this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) translateZ(0)';
-    });
-});
+//     card.addEventListener('mouseleave', function() {
+//         this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) translateZ(0)';
+//     });
+// });
